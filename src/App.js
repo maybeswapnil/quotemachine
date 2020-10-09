acdaf170d6dba0d3cf1238de582aa061951c96ae
Swapnil Sharma <swapnil.sharma1998@outlook.com>
2020-10-09 21:01:15 +0530

    dsf

--- src/App.js
+++ src/App.js
@@ -1,24 +1,27 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+var dataset = [];
+var quote = [];
+var author = [];
+const request = new XMLHttpRequest();
+request.open("GET", "https://gist.githubusercontent.com/nasrulhazim/54b659e43b1035215cd0ba1d4577ee80/raw/e3c6895ce42069f0ee7e991229064f167fe8ccdc/quotes.json", true)
+request.onload = () => {
+    dataset = JSON.parse(request.responseText).quotes;
+    quote = dataset.map((e)=>(e.quote)).filter((j)=>(j.length<90));
+    author = dataset.map((e)=>(e.author));
+    App();
+}
+request.send();
 
 function App() {
+  var f = 0; 
+  f = Math.floor(Math.random()*10)
+  console.log(quote[f]);
+  console.log(author[f]);
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <p>{quote[f]}</p>
+      <p id="writer">~{author[f]}</p>
     </div>
   );
 }
--- src/index.js
+++ src/index.js
@@ -1,17 +1,57 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
+import { render } from 'react-dom';
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
+const colors = ['#16a085',
+                '#27ae60',
+                '#2c3e50',
+                '#f39c12',
+                '#e74c3c',
+                '#9b59b6',
+                '#FB6964',
+                '#342224',
+                '#472E32',
+                '#BDBB99',
+                '#77B1A9',
+                '#73A857'];
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+class Controller extends React.Component {
+    
+    constructor(props) {
+        super(props);
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
+        this.state = {
+            backgroundColor: colors[0],
+            color : colors[0],
+            transitionDuration: "0.8s"
+        };
+        this.reload = this.reload.bind(this);
+    }
+
+    reload() {
+        console.log(colors[Math.floor(Math.random()*10)]);
+        var c = Math.floor(Math.random()*10);
+        this.setState(state => ({
+            backgroundColor: colors[c],
+            color: colors[c]
+          }));
+    }
+
+    render() { 
+        return (
+            <div className="Main" style={this.state}>
+                <div className="Box">
+                    <p id="tag" style={{color: this.state.color}}>»</p>
+                    <p id="quote" style={{color: this.state.color}}><App /></p>
+                    <button id="getNew" style={{color: this.state.backgroundColor}} onClick={this.reload}>New Quote</button>
+                </div>
+            </div>
+        );
+    }
+}
+
+render(<Controller />, document.getElementById("root"))
+
+
 serviceWorker.unregister();
